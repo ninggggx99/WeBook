86e56abf55c9af46020e9cc3d8d0f8cfdea98e11
ninggggx99 <qingning99@gmail.com>
2020-05-27 11:20:32 +0800

    Login Provider done, Login not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:webookapp/view/LandingPage.dart';
+import 'package:webookapp/view/home.dart';
+import 'package:webookapp/view/login_signup_view.dart';
 import 'package:webookapp/view_model/auth_provider.dart';
 
 void main() {
@@ -14,13 +16,14 @@ class MyApp extends StatelessWidget {
 
   return MultiProvider(
         providers: [
-          Provider(create: (_) => AuthProvider()),
+          ChangeNotifierProvider(create: (_) => AuthProvider()),
         ],
         child: MaterialApp(
             title: 'WeBook',
             theme: ThemeData(
               primarySwatch: Colors.teal,
             ),
+            // home: context.watch<AuthProvider>().status == AuthStatus.LOGGED_IN? HomePage():LoginSignupPage(),
             home: LandingPage(),
             )  
         );
--- lib/view/LandingPage.dart
+++ lib/view/LandingPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:webookapp/database/authentication.dart';
 import 'package:webookapp/view/home.dart';
 import 'package:webookapp/view/login_signup_view.dart';
 import 'package:webookapp/view_model/auth_provider.dart';
@@ -9,7 +10,8 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // retrieve firebaseAuth from above in the widget tree
-    final auth = Provider.of<AuthProvider>(context,listen: true);
+    
+    final auth = Provider.of<AuthProvider>(context);
     if (auth.status == AuthStatus.LOGGED_IN){
       return HomePage();
     }
